e2805f75c0e5e22eefac7fb24fdaec408480178c
Dnyanesh <dnyaneshwar.shejul990@gmail.com>
2022-12-29 10:12:19 +0530

    first commit

---
+++ GitDemo/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		if(i==0) {
+			System.out.println("i is 0");
+		}
+		else {
+			System.out.println("i is 1");
+		}
+			
+	}
+}
